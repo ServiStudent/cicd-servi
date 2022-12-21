5de1dd0efaf0a908c40e27db65d3a8f95fdf99f9
Huijbregts,Servi S.P.G.I <servi.huijbregts@student.fontys.nl>
2022-12-21 23:30:05 +0100

    Sonarcloud + Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+const sum = require('./sum');
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('adds 2 + 5 to equal 7', () => {
+  expect(sum(2, 5)).toBe(7);
 });
